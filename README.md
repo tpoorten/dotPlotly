c2d7a4ed7135304a3588803bd15390f003232767
tpoorten <tom.poorten@gmail.com>
2017-05-15 17:53:15 -0700

    add back step 1 of queryID sorting, add plotting options, add verbose option

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ R script that makes a plotly interactive dot plot. The input is the coords file 
 Example:
 ```
 show-coords -c example.delta > example.coords
-./mummerCoordsDotPlotly.R -i example.coords -k 4 -q 300000 -m 10000 -o out
+./mummerCoordsDotPlotly.R -i example.coords -o out -s -m 6000 -q 100000 -k 7
 ```
 
 The script requires three R packages: `install.packages(c("optparse", "ggplot2", "plotly"))`. 
@@ -19,11 +19,20 @@ Use `./mummerCoordsDotPlotly.R -h` to see options.
 	-o OUTPUT, --output=OUTPUT
 		output filename prefix [default out]
 
+	-v, --verbose
+		Print out all parameter settings [default]
+
 	-q MIN-QUERY-LENGTH, --min-query-length=MIN-QUERY-LENGTH
 		filter queries with total alignments less than cutoff X bp [default 4e+05]
 
 	-m MIN-ALIGNMENT-LENGTH, --min-alignment-length=MIN-ALIGNMENT-LENGTH
 		filter alignments less than cutoff X bp [default 10000]
+
+	-p PLOT-SIZE, --plot-size=PLOT-SIZE
+		plot size X by X inches [default 15]
+
+	-l, --show-horizontal-lines
+		turn on horizontal lines on plot for separating scaffolds  [default FALSE]
 
 	-k NUMBER-REF-CHROMOSOMES, --number-ref-chromosomes=NUMBER-REF-CHROMOSOMES
 		number of sorted reference chromosomes to keep [default all chromosmes]
@@ -32,7 +41,7 @@ Use `./mummerCoordsDotPlotly.R -h` to see options.
 		turn on color alignments by percent similarity [default FALSE]
 
 	-x, --interactive-plot-off
-		turn off production of interactive plotly [default FALSE]
+		turn off production of interactive plotly [default TRUE]
 
 	-h, --help
 		Show this help message and exit
--- mummerCoordsDotPlotly.R
+++ mummerCoordsDotPlotly.R
@@ -11,19 +11,27 @@ option_list <- list(
   make_option(c("-o","--output"), type="character", default="out",
               help="output filename prefix [default %default]",
               dest="output_filename"),
+  make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
+              help="Print out all parameter settings [default]"),
   make_option(c("-q", "--min-query-length"), type="numeric", default=400000,
               help="filter queries with total alignments less than cutoff X bp [default %default]",
               dest="min_query_aln"),
   make_option(c("-m", "--min-alignment-length"), type="numeric", default=10000,
               help="filter alignments less than cutoff X bp [default %default]",
               dest="min_align"),
+  make_option(c("-p","--plot-size"), type="numeric", default=15,
+              help="plot size X by X inches [default %default]",
+              dest="plot_size"),
+  make_option(c("-l", "--show-horizontal-lines"), action="store_true", default=FALSE,
+              help="turn on horizontal lines on plot for separating scaffolds  [default %default]",
+              dest="h_lines"),
   make_option(c("-k", "--number-ref-chromosomes"), type="numeric", default=NULL,
               help="number of sorted reference chromosomes to keep [default all chromosmes]",
               dest="keep_ref"),
   make_option(c("-s", "--similarity"), action="store_true", default=FALSE,
               help="turn on color alignments by percent similarity [default %default]",
               dest="similarity"),
-  make_option(c("-x", "--interactive-plot-off"), action="store_true", default=FALSE,
+  make_option(c("-x", "--interactive-plot-off"), action="store_false", default=TRUE,
               help="turn off production of interactive plotly [default %default]",
               dest="interactive")
 )
@@ -32,9 +40,18 @@ options(error=traceback)
 
 parser <- OptionParser(usage = "%prog -i alignments.coords -o out [options]",option_list=option_list)
 opt = parse_args(parser)
-# opt = list(input_filename="FvescaV4_v_p_ctg.nucmer.coords", output_filename="out", min_align=15000, min_query_aln = 300000, keep_ref=7, similarity=TRUE)
-# print(opt)
 
+if(opt$v){
+  cat(paste0("PARAMETERS:\ninput (-i): ", opt$input_filename,"\n"))
+  cat(paste0("output (-o): ", opt$output_filename,"\n"))
+  cat(paste0("minimum query aggregate alignment length (-q): ", opt$min_query_aln,"\n"))
+  cat(paste0("minimum alignment length (-m): ", opt$min_align,"\n"))
+  cat(paste0("plot size (-p): ", opt$plot_size,"\n"))
+  cat(paste0("show horizontal lines (-l): ", opt$h_lines,"\n"))
+  cat(paste0("number of reference chromosomes to keep (-k): ", opt$keep_ref,"\n"))
+  cat(paste0("show similarity (-s): ", opt$similarity,"\n"))
+  cat(paste0("produce interactive plot (-x): ", opt$interactive,"\n\n"))
+}
 # read in alignments
 alignments = read.table(opt$input_filename, stringsAsFactors = F, skip = 5)
 alignments = alignments[,-c(3,6,9,11,14)]
@@ -42,8 +59,8 @@ alignments = alignments[,-c(3,6,9,11,14)]
 # set column names
 colnames(alignments) = c("refStart","refEnd","queryStart","queryEnd","lenAlnRef","lenAlnQuery","percentID","percentAlnCovRef","percentAlnCovQuery","refID","queryID")
 
-print(paste("Number of alignments:", nrow(alignments)))
-print(paste("Number of query sequences:", length(unique(alignments$queryID))))
+cat(paste0("Number of alignments: ", nrow(alignments),"\n"))
+cat(paste0("Number of query sequences: ", length(unique(alignments$queryID)),"\n"))
 
 # filter queries by alignment length, for now include overlapping intervals
 queryLenAgg = tapply(alignments$lenAlnQuery, alignments$queryID, sum)
@@ -65,8 +82,8 @@ if(is.null(opt$keep_ref)){
   alignments = alignments[which(alignments$refID %in% names(sort(chromMax, decreasing = T)[1:opt$keep_ref])),]
 }
 
-print(paste("After filtering... Number of alignments:", nrow(alignments)))
-print(paste("After filtering... Number of query sequences:", length(unique(alignments$queryID))))
+cat(paste0("\nAfter filtering... Number of alignments: ", nrow(alignments),"\n"))
+cat(paste0("After filtering... Number of query sequences: ", length(unique(alignments$queryID)),"\n\n"))
 
 # sort df on ref
 alignments$refID = factor(alignments$refID, levels = names(sort(chromMax, decreasing = T)[1:opt$keep_ref])) # set order of refID
@@ -81,12 +98,27 @@ chromMax = tapply(alignments$refEnd, alignments$refID, max)
 alignments$refStart2 = alignments$refStart + sapply(as.character(alignments$refID), function(x) ifelse(x == names(sort(chromMax, decreasing = T))[1], 0, cumsum(chromMax)[match(x, names(sort(chromMax, decreasing = T))) - 1]) )
 alignments$refEnd2 = alignments$refEnd +     sapply(as.character(alignments$refID), function(x) ifelse(x == names(sort(chromMax, decreasing = T))[1], 0, cumsum(chromMax)[match(x, names(sort(chromMax, decreasing = T))) - 1]) )
 
+## queryID sorting step 1/2
+# sort levels of factor 'queryID' based on longest alignment
+alignments$queryID = factor(alignments$queryID, levels=unique(as.character(alignments$queryID))) 
+queryMaxAlnIndex = tapply(alignments$lenAlnQuery,
+                          alignments$queryID,
+                          which.max,
+                          simplify = F)
+alignments$queryID = factor(alignments$queryID, levels = unique(as.character(alignments$queryID))[order(mapply(
+  function(x, i)
+    alignments$refStart2[which(i == alignments$queryID)][x],
+  queryMaxAlnIndex,
+  names(queryMaxAlnIndex)
+))])
+
+## queryID sorting step 2/2
 ## sort levels of factor 'queryID' based on longest aggregrate alignmentst to refID's
 # per query ID, get aggregrate alignment length to each refID 
-queryLenAggPerRef = sapply(unique(as.character(alignments$queryID)), function(x) tapply(alignments$lenAlnQuery[which(alignments$queryID == x)], alignments$refID[which(alignments$queryID == x)], sum) )
+queryLenAggPerRef = sapply((levels(alignments$queryID)), function(x) tapply(alignments$lenAlnQuery[which(alignments$queryID == x)], alignments$refID[which(alignments$queryID == x)], sum) )
 queryID_Ref = apply(queryLenAggPerRef, 2, function(x) rownames(queryLenAggPerRef)[which.max(x)])
 # set order for queryID
-alignments$queryID = factor(alignments$queryID, levels = unique(as.character(alignments$queryID))[order(match(queryID_Ref, levels(alignments$refID)))])
+alignments$queryID = factor(alignments$queryID, levels = (levels(alignments$queryID))[order(match(queryID_Ref, levels(alignments$refID)))])
 
 #  flip query starts stops to forward if most align are in reverse complement
 queryRevComp = tapply(alignments$queryEnd - alignments$queryStart, alignments$queryID, function(x) sum(x)) < 0
@@ -141,9 +173,9 @@ if (opt$similarity) {
       axis.text.y = element_text(size = 4, angle = 15)
     ) +
     scale_y_continuous(breaks = yTickMarks, labels = levels(alignments$queryID)) +
-    geom_hline(yintercept = yTickMarks,
+    { if(opt$h_lines){ geom_hline(yintercept = yTickMarks,
                color = "grey60",
-               size = .1) +
+               size = .1) }} +
     scale_color_distiller(palette = "Spectral") +
     labs(color = "Mean Percent Identity (per query)") +
     xlab("Reference") +
@@ -151,9 +183,9 @@ if (opt$similarity) {
 } else {
   gp = ggplot(alignments) +
     geom_point(mapping = aes(x = refStart2, y = queryStart2),
-               size = 0.015) +
+               size = 0.009) +
     geom_point(mapping = aes(x = refEnd2, y = queryEnd2),
-               size = 0.015) +
+               size = 0.009) +
     geom_segment(aes(
       x = refStart2,
       xend = refEnd2,
@@ -177,17 +209,17 @@ if (opt$similarity) {
       axis.text.y = element_text(size = 4, angle = 15)
     ) +
     scale_y_continuous(breaks = yTickMarks, labels = levels(alignments$queryID)) +
-    geom_hline(yintercept = yTickMarks,
-               color = "grey60",
-               size = .1) +
+    { if(opt$h_lines){ geom_hline(yintercept = yTickMarks,
+                                  color = "grey60",
+                                  size = .1) }} +
     labs(color = "Mean Percent Identity (per query)") +
     xlab("Reference") +
     ylab("Query")
 }
 # gp
-ggsave(filename = paste0(opt$output_filename, ".pdf"), width = 10, height = 10)
+ggsave(filename = paste0(opt$output_filename, ".png"), width = opt$plot_size, height = opt$plot_size, units = "in")
 
-if(!opt$interactive){
+if(opt$interactive){
   pdf(NULL)
   gply = ggplotly(gp, tooltip = "text")
   htmlwidgets::saveWidget(as.widget(gply), file = paste0(opt$output_filename, ".html"))
